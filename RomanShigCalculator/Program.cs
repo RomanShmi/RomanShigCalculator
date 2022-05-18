481c05c70f74adfa09054d7414aa0ebb3a2bb5e9
Roman Shmigelskyi <romyli4ka@gmail.com>
2022-05-18 13:41:06 -0400

    program is done

--- RomanShigCalculator/Program.cs
+++ RomanShigCalculator/Program.cs
@@ -1,2 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("please enter two numbers and operator +  -  *  /");
+int num1= Convert.ToInt16( Console.ReadLine());
+int num2 = Convert.ToInt16(Console.ReadLine());
+string ope = Console.ReadLine();
+
+bool yes = true;
+
+while (yes)
+{
+    switch (ope)
+    {
+        case "+":
+            Console.WriteLine(num1+num2);
+            yes = false;
+            break;
+        case "-":
+            Console.WriteLine(num1 - num2);
+            yes = false;
+            break;
+        case "*":
+            Console.WriteLine(num1 * num2);
+            yes = false;
+            break;
+        case "/":
+            Console.WriteLine(num1 / num2);
+            yes = false;
+            break;
+        default:
+            Console.WriteLine("Wrong operator put another operator +  -  *  /");
+            ope = Console.ReadLine();
+            break;
+    }
+
+
+
+
+
+
+
+}
